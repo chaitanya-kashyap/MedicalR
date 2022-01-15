a86857b7f36d7d8fedd85e2195627ec3ac3988c5
Chaitanya Kashyap <ckashyap500@gmail.com>
2022-01-15 22:29:41 +0530

    updated

--- components/header.jsx
+++ components/header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
                 <div className="flex items-center justify-between h-16">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <Link href='/'>
+                      <Link passhref='/' passHref={true}>
                       <Image
                         className="h-8 w-8"
                         src={R}
