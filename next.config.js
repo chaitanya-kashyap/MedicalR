e6c6d55b10d74b53a1386bd49c60331884babd97
Chaitanya Kashyap <ckashyap500@gmail.com>
2022-01-21 20:57:01 +0530

    The road to PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
