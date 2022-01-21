e4aaff3ce45e52974e43d07b7f67aadd5506c4bc
Chaitanya Kashyap <55341996+chaitanya-kashyap@users.noreply.github.com>
2022-01-21 16:22:02 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
     providers: [
       GoogleProvider({
-        clientId: "process.env.GOOGLE_CLIENT_ID",
-        clientSecret: "process.env.GOOGLE_SECRET",
+        clientId: process.env.GOOGLE_CLIENT_ID
+        clientSecret: process.env.GOOGLE_SECRET
         authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
       })
     ],
