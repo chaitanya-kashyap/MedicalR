d4e90137e91a8dcc354db75b6632644f594f7659
Chaitanya Kashyap <ckashyap500@gmail.com>
2022-01-28 18:11:42 +0530

    Connected

--- pages/home/index.js
+++ pages/home/index.js
@@ -47,7 +47,7 @@ export default function UserHome({ patients }) {
                 <tr>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 pl-10 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Name
                   </th>
@@ -81,9 +81,6 @@ export default function UserHome({ patients }) {
                   <tr key={patient.name}>
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10">
-                          <img className="h-10 w-10 rounded-full" src={patient.image} alt="" />
-                        </div>
                         <div className="ml-4">
                           <div className="text-sm font-medium text-gray-900">{patient.name}</div>
                           <div className="text-sm text-gray-500">{patient.mobile}</div>
